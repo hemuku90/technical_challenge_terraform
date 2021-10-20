ce42cb1a17a883f02d1d70149aad4f0a0813a75c
Hemant Kumar <hkumar@Ten-C02G828YMD6M.local>
2021-10-20 09:22:16 +0530

    removed special char support

--- connection_string_module/main.tf
+++ connection_string_module/main.tf
@@ -1,7 +1,7 @@
 resource "random_password" "password" {
   count            = 2
   length           = 16
-  special          = true
+  special          = false
   override_special = "_%@"
 }
 ##############################################################
